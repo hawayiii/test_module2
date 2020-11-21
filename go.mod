daa20ed29cc312feb2c8d3227dca9822a66e37bc
weyi he <weiyi.he@shopee.com>
2020-11-22 00:06:00 +0800

    add say.go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hawayiii/test_module2
+
+go 1.14
---
+++ say.go
@@ -0,0 +1,15 @@
+package teset_module2
+
+import "fmt"
+
+func Hello() {
+	fmt.Println("first commit")
+}
+
+func Hello2() {
+	fmt.Println("second commit")
+}
+
+func HelloV1() {
+	fmt.Println("hello v1.0.0")
+}
